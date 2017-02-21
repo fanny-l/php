e08a5ce0df18f7d0158b352e8902af8868de8487
fanny-l <lochufanny@gmail.com>
2017-02-21 20:23:14 +0100

    Create connexion.php

---
+++ connexion.php
@@ -0,0 +1,18 @@
+<?php
+$host 		  = "localhost";
+$username 	= "root";
+$password 	= "root";
+$dbname 	  = "yourdatabase";
+
+try
+{
+	$db = new PDO('mysql:host='.$host.';dbname='.$dbname, $username, $password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected successfully"; 
+}
+catch(Exception $e)
+{
+	echo 'Une erreur est survenue !';
+	die();
+}
+?>
