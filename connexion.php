373dba67ab971abc18f2381a6f39c574b3ad183d
fanny-l <lochufanny@gmail.com>
2017-02-21 20:39:09 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -8,7 +8,7 @@ try
 {
 	$db = new PDO('mysql:host='.$host.';dbname='.$dbname, $username, $password);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "Connected successfully"; 
+	echo "Connection réussie !"; 
 }
 catch(Exception $e)
 {
