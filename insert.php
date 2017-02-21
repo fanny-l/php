deb3813ee8755cd2fffc084c6adce5a7da801bf5
fanny-l <lochufanny@gmail.com>
2017-02-21 21:17:20 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,11 @@
+try
+{
+	$req_insert = 'INSERT INTO nomdematable 
+   SET colonne1 = "valeur1", colonne2 = "valeur2", colonne3 = "valeur3"';
+	$sql_insert = $db->prepare($req_insert);
+	$sql_insert->execute();
+}
+catch(PDOException $ex)
+{
+	die("Erreur " . $ex->getMessage());
+}
