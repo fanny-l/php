4027ef49aae2fa30412e6456c3957d21d41522c3
fanny-l <lochufanny@gmail.com>
2017-02-22 11:46:25 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,10 +3,10 @@ include('connexion.php');
 
 try
 {
-	$req_insert = 'INSERT INTO nomdematable 
-   		       SET colonne1 = "valeur1", 
-		       colonne2 = "valeur2", 
-		       colonne3 = "valeur3"';
+	$req_insert = "INSERT INTO nomdematable 
+   		       SET colonne1 = 'valeur1', 
+		       colonne2 = 'valeur2', 
+		       colonne3 = 'valeur3'";
 	$sql_insert = $db->prepare($req_insert);
 	$sql_insert->execute();
 }
