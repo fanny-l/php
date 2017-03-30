14fbd10f0a7431b651f7d26f83e5dbdf371cf371
fanny-l <lochufanny@gmail.com>
2017-03-30 13:40:00 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,13 @@
 <?php
 try
 {
-	$req_delete = "DELETE FROM nomdematable 
-				   WHERE id = ".$_GET['id'];
-	$sql_delete = $db->prepare($req_delete);
-	$sql_delete->execute();
+     $req_delete = "DELETE FROM nomdematable 
+		    WHERE id = ".$_GET['id'];
+     $sql_delete = $db->prepare($req_delete);
+     $sql_delete->execute();
 }
 catch(PDOException $ex)
 {
-	die("Erreur " . $ex->getMessage());
+     die("Erreur " . $ex->getMessage());
 }
 ?>
