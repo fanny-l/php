1c1bf0d1ec4547457482acd13b6475175582d8ca
fanny-l <lochufanny@gmail.com>
2017-03-30 13:19:13 +0200

    Create select-where.php

---
+++ select-where.php
@@ -0,0 +1,18 @@
+<?php
+  // Sélection des données
+  try
+  {
+    $req_films = "SELECT colonne1, colonne2 
+     			  FROM nomdematable
+     			  WHERE id = ".$_GET['id'];
+    $sql_films = $db->prepare($req_films);
+    $sql_films->execute();
+    $ligne_films = $sql_films->fetch();
+  }
+  catch(PDOException $ex)
+  {
+    die("Erreur " . $ex->getMessage());
+  }
+?>
+	<!-- Exemple d'affichage des données dans un chamo de formulaire -->
+	<input type="text" name="nom" value="<?php echo $ligne_films['nom']; ?>">
