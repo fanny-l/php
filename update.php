71c0aba0372236a5f6e88a96c88c2c22895821eb
fanny-l <lochufanny@gmail.com>
2017-03-30 13:27:41 +0200

    Create update.php

---
+++ update.php
@@ -0,0 +1,14 @@
+<?php
+try
+{
+	$req_update = "UPDATE nomdematable 
+   				   SET colonne1 = 'valeur1', colonne2 = 'valeur2', colonne3 = 'valeur3'
+				   WHERE id = ".$_POST['id'];
+	$sql_update = $db->prepare($req_update);
+	$sql_update->execute();
+}
+catch(PDOException $ex)
+{
+	die("Erreur " . $ex->getMessage());
+}
+?>
