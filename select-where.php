0b05f289cc3d968bd6a6805710182cb9f5918059
fanny-l <lochufanny@gmail.com>
2017-03-30 13:20:09 +0200

    Update select-where.php

--- select-where.php
+++ select-where.php
@@ -3,8 +3,8 @@
   try
   {
     $req_films = "SELECT colonne1, colonne2 
-     			  FROM nomdematable
-     			  WHERE id = ".$_GET['id'];
+     		  FROM nomdematable
+     		  WHERE id = ".$_GET['id'];
     $sql_films = $db->prepare($req_films);
     $sql_films->execute();
     $ligne_films = $sql_films->fetch();
@@ -14,5 +14,6 @@
     die("Erreur " . $ex->getMessage());
   }
 ?>
-	<!-- Exemple d'affichage des données dans un chamo de formulaire -->
-	<input type="text" name="nom" value="<?php echo $ligne_films['nom']; ?>">
+
+<!-- Exemple d'affichage des données dans un chamo de formulaire -->
+<input type="text" name="nom" value="<?php echo $ligne_films['nom']; ?>">
